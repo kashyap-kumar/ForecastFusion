07bcfa49d5663a1fd301bfce629c1f9e2055d6f6
Kashyap Kumar <66243247+kashyap-kumar@users.noreply.github.com>
2024-01-25 23:29:49 +0530

    FEAT: Show hourly data using charts

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { WiDayCloudy } from "react-icons/wi"
+import { WiDayCloudy, WiStrongWind, WiThermometerExterior } from "react-icons/wi"
 import { TiArrowDown, TiArrowUp } from "react-icons/ti"
+import { Line, LineChart, ResponsiveContainer, XAxis, Tooltip, BarChart, Bar } from "recharts";
+import { getHourlyData } from "../utils/api";
+
+// test data for various time during the day (due to api limitations)
+const hourlyData = getHourlyData();
 
 const CurrentWeather = ({ weatherData }) => {
     console.log(weatherData);
+    const [selectedOverview, setSelectedOverview] = useState("temp"); // posible values: temp and wind
+
     return (
         <Row>
             <Col lg={8}>
                 <Row>
                     <Col sm={6} className="mb-2">
-                        <div className="rounded-5 p-4" style={{backgroundImage: "url('/src/assets/weather-info-bg.jpg')", backgroundSize: "cover"}}>
-                            <div><WiDayCloudy size={70} color={'white'} style={{textShadow: "0 4px #000"}}/></div>
-                            <div className="text-white" style={{textShadow: "rgba(0, 0, 0, 0.4) 0px 0px 5px, rgba(0, 0, 0, 0.2) 0px 4px 2px"}}>
+                        <div
+                            className="rounded-5 p-4"
+                            style={{
+                                backgroundImage: "url('/src/assets/weather-info-bg.jpg')",
+                                backgroundSize: "cover",
+                            }}
+                        >
+                            <div>
+                                <WiDayCloudy size={70} color={"white"} style={{ textShadow: "0 4px #000" }} />
+                            </div>
+                            <div
+                                className="text-white"
+                                style={{ textShadow: "rgba(0, 0, 0, 0.4) 0px 0px 5px, rgba(0, 0, 0, 0.2) 0px 4px 2px" }}
+                            >
                                 <span className="display-3 fw-bold">{weatherData.main.temp}</span>
                                 {"°C"}
                                 <p className="fs-4 fw-medium">{weatherData.weather[0].description}</p>
                             </div>
-                            <div className="bg-light d-flex justify-content-between rounded-5 px-3 py-1" style={{fontSize: "14px"}}>
+                            <div
+                                className="bg-light d-flex justify-content-between rounded-5 px-3 py-1"
+                                style={{ fontSize: "14px" }}
+                            >
                                 <p className="mb-0">Feels like {weatherData.main.feels_like}°C</p>
                                 <p className="mb-0">
-                                    <span className="me-2"><TiArrowDown /> {weatherData.main.temp_min}</span>
-                                    <span><TiArrowUp /> {weatherData.main.temp_max}</span>
+                                    <span className="me-2">
+                                        <TiArrowDown /> {weatherData.main.temp_min}
+                                    </span>
+                                    <span>
+                                        <TiArrowUp /> {weatherData.main.temp_max}
+                                    </span>
                                 </p>
                             </div>
                         </div>
                     </Col>
                     <Col sm={6} className="mb-2">
-                        <div className="rounded-5 p-4 h-100" style={{backgroundImage: "url('/src/assets/weather-more-info-bg.png')", backgroundSize: "cover"}}>
+                        <div
+                            className="rounded-5 p-4 h-100"
+                            style={{
+                                backgroundImage: "url('/src/assets/weather-more-info-bg.png')",
+                                backgroundSize: "cover",
+                            }}
+                        >
                             <p className="d-flex justify-content-between bg-white rounded-4 py-2 px-3">
                                 <span>Humidity</span>
                                 <span>{weatherData.main.humidity}%</span>
                             </p>
                             <p className="d-flex justify-content-between bg-white rounded-4 py-2 px-3">
                                 <span>Visibility</span>
-                                <span>{weatherData.visibility/1000}km</span>
+                                <span>{weatherData.visibility / 1000}km</span>
                             </p>
                             <p className="d-flex justify-content-between bg-white rounded-4 py-2 px-3">
                                 <span>Pressure</span>
@@ -42,17 +73,104 @@ const CurrentWeather = ({ weatherData }) => {
                             </p>
                             <p className="d-flex justify-content-between bg-white rounded-4 py-2 px-3">
                                 <span>Wind</span>
-                                <span>{(weatherData.wind.speed*3.6).toFixed(2)} km/hr</span>
+                                <span>{(weatherData.wind.speed * 3.6).toFixed(2)} km/hr</span>
                             </p>
                         </div>
                     </Col>
-                    <Col sm={12} className="mb-2">Charts</Col>
+                    <Col sm={12} className="mb-2 mt-md-3">
+                        <div className="rounded-5 p-4 h-100" style={{ backgroundColor: "#edf0f0" }}>
+                            <Row>
+                                <Col sm={12}>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <p className="mb-0">Overview of the day</p>
+                                        <p className="mb-0 d-flex align-items-center">
+                                            <span
+                                                className="d-inline-flex justify-content-center align-items-center me-2 rounded fs-2 p-1"
+                                                style={{
+                                                    backgroundColor: selectedOverview === "temp" ? "#f7703b" : "#c2c9c9",
+                                                    color: selectedOverview === "temp" ? "#ffffff" : "#626669",
+                                                    width: "30px",
+                                                    height: "30px",
+                                                }}
+                                                onClick={() => setSelectedOverview("temp")}
+                                            >
+                                                <WiThermometerExterior />
+                                            </span>
+                                            <span
+                                                className="d-inline-flex justify-content-center align-items-center rounded fs-2 p-1"
+                                                style={{
+                                                    backgroundColor: selectedOverview === "wind" ? "#f7703b" : "#c2c9c9",
+                                                    color: selectedOverview === "wind" ? "#ffffff" : "#626669",
+                                                    width: "30px",
+                                                    height: "30px",
+                                                }}
+                                                onClick={() => setSelectedOverview("wind")}
+                                            >
+                                                <WiStrongWind />
+                                            </span>
+                                        </p>
+                                    </div>
+                                </Col>
+                                <Col sm={12}>
+                                    <div className="_chart-container" style={{ width: "100%", height: "200px" }}>
+                                        <ResponsiveContainer width="100%" height="100%">
+                                            {/* show temp chart or wind chart based on selected tab */}
+
+                                            {selectedOverview === "temp" && (
+                                                <LineChart
+                                                    width={500}
+                                                    height={300}
+                                                    data={hourlyData}
+                                                    margin={{
+                                                        top: 25,
+                                                        right: 30,
+                                                        left: 20,
+                                                        bottom: 5,
+                                                    }}
+                                                >
+                                                    <XAxis dataKey="time" tickLine={false} />
+                                                    <Tooltip
+                                                        contentStyle={{ background: "transparent", border: "none" }}
+                                                        labelStyle={{ display: "none" }}
+                                                        separator=""
+                                                        formatter={(value) => [`${value}°C`, ""]}
+                                                    />
+                                                    <Line type="monotone" dataKey="temp" stroke="#f7703b" dot={false} />
+                                                </LineChart>
+                                            )}
+
+                                            {selectedOverview === "wind" && (
+                                                <BarChart
+                                                    width={500}
+                                                    height={300}
+                                                    data={hourlyData}
+                                                    margin={{
+                                                        top: 25,
+                                                        right: 30,
+                                                        left: 20,
+                                                        bottom: 5,
+                                                    }}
+                                                >
+                                                    <XAxis dataKey="time" tickLine={false} axisLine={false} />
+                                                    <Bar dataKey="wind" fill="#f7703b" label={{ position: "top" }} />
+                                                </BarChart>
+                                            )}
+                                        </ResponsiveContainer>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
                 </Row>
             </Col>
             <Col lg={4}>
                 <Row>
-                    <Col sm={6} lg={12}>Sunrise and sunset</Col>
-                    <Col sm={6} lg={12}>Weather alerts</Col>
+                    <Col sm={6} lg={12}>
+                        Sunrise and sunset
+                    </Col>
+                    <Col sm={6} lg={12}>
+                        Weather alerts
+                    </Col>
                 </Row>
             </Col>
         </Row>
--- src/utils/api.js
+++ src/utils/api.js
@@ -23,3 +23,24 @@ export const getForecast = async (lat, lon) => {
     
     return data;
 }
+
+// NOTE: FAKE DATA because of API limitation to provide hourly data
+export const getHourlyData = () => {
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    const windDirections = ['north', 'north-east', 'east', 'south-east', 'south', 'south-west', 'west', 'north-west'];
+
+    let weatherData = [];
+
+    for (let i = 1; i < 12; i++) {
+        let weatherObject = {
+            time: `${i}PM`,
+            temp: getRandomInt(10, 30), // Assuming temperature range between 10 and 30
+            cloud: getRandomInt(1, 3),
+            wind: getRandomInt(5, 20), // Assuming wind speed range between 5 and 20
+            direction: windDirections[getRandomInt(0, 7)] // Randomly select a wind direction
+        };
+        weatherData.push(weatherObject);
+    }
+
+    return weatherData;
+}
